98042047be1f603957082c1c368df51f1755dfbc
phandat128 <phandat12082002@gmail.com>
2022-07-19 23:04:44 +0700

    update

--- http-collect.js
+++ http-collect.js
@@ -5,10 +5,10 @@ const url = process.argv[2]
 http.get(url, callback)
 
 function callback(response){
-    response.setEncoding('utf8')
+    // response.setEncoding('utf8')
     response.pipe(bl(function(err, data){
         const str = data.toString()
+        console.log(str.length)
         console.log(str)
-        console.log(typeof str)
     }))
 }
